6b81aca1d5c66d4e2ce4d59d43f5a0b692a1daf9
Rizki Ashari <80566147+rizkiashari@users.noreply.github.com>
2022-06-02 10:59:08 +0700

    Fix Bug

--- BE/migrations/20210731094220-create-orders.js
+++ BE/migrations/20210731094220-create-orders.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Orders", {
+    await queryInterface.createTable("orders", {
       id: {
         allowNull: false,
         autoIncrement: true,
